5be147836ad7b36d8945c6d1b74ca734e3e6a9f0
CeliaMeli05 <132369129+CeliaMeli05@users.noreply.github.com>
2023-05-02 16:43:46 +0200

    Ajout des fichiers

---
+++ quest.js
@@ -0,0 +1,24 @@
+function check() {
+	window.alert("La bonne réponse est : Poséidon"); 
+	
+}
+ function check_02() {
+ 	window.alert("La bonne réponse est : OEdipe"); 
+
+ }
+
+function check_03() {
+ 	window.alert("La bonne réponse est : Hippolyte"); 
+ }
+
+function check_04() {
+ 	window.alert("La bonne réponse est : Avec un pieu"); 
+ }
+
+ function check_05() {
+ 	window.alert("La bonne réponse est : Polynice"); 
+ }
+
+ function check_06() {
+ 	window.alert("La bonne réponse est : 2"); 
+ }
